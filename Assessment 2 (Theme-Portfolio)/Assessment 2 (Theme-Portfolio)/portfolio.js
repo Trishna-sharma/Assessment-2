f45a3f51c489e4cf9caaa9405dd34c0461b0f230
Trishna-sharma <91663993+Trishna-sharma@users.noreply.github.com>
2022-12-03 22:16:41 +0400

    Portfolio

---
+++ Assessment 2 (Theme-Portfolio)/Assessment 2 (Theme-Portfolio)/portfolio.js
@@ -0,0 +1,21 @@
+﻿objectFitVideos();
+
+
+src = "https://github.com/TricomB2B/object-fit-videos/blob/master/object-fit-videos.js"
+
+src = "https://cdn.jsdelivr.net/npm/typed.js@2.0.12"
+
+
+var typed = new Typed(".auto-type", {
+    strings: [, "Creative computing student", "UI/UX Designer",
+        "Web developer"],
+    typeSpeed: 150,
+    backSpeed: 150,
+    loop: true
+})
+
+function reset() {
+	location.reload();
+}
+
+
